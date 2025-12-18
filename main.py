6ff609ecd173be0dccb3bca90fd7bfa48e42f72e
Vnn-C <vannienguyen993@gmail.com>
2025-12-18 12:50:40 -0600

    Edited authorization process to store credentials locally

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from dotenv import load_dotenv, dotenv_values
 import LikedVids
 import PlaylistItems
 import webbrowser
+from datetime import datetime, timedelta
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
 
 load_dotenv()
 
@@ -30,6 +33,10 @@ def main():
     api_version = "v3"
     DEVELOPER_KEY = os.getenv("API_KEY")
     client_secrets_file = os.getenv("CLIENT_SECRET")  
+    #auth_token = os.getenv("AUTH_TOKEN")
+    #refresh_token = os.getenv("REFRESH_TOKEN")
+    #token_added_time = os.getenv("TOKEN_ADDED_TIME")
+    #stored_credentials = os.getenv("CREDENTIALS")
 
 #TODO Started at 5/17/2025: Configure script to open Google Chrome for authentication. Problem: The authentication opens in Microsoft Edge by default
 #Could not accomplish due to how flow.run_local_server() works. Changing the default browser to Google Chrome in Settings can resolve the problem
@@ -45,15 +52,47 @@ def main():
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
             client_secrets_file, scopes)
 
+#TODO: 12/18/2025 Store credential's auth token and refresh token or the credentials itself to eliminiate the need for reauthorizing myself every session
 #flow.run_local_server is not present in the Youtube API's code sample, this code was changed to allow the script to run on a laptop
-    credentials = flow.run_local_server(port=0, open_browser=True)
-    #flow.run_local_console does not work
+    
+    #Old code in case 12/18/2025 todo does not work
+    #credentials = flow.run_local_server(port=0, open_browser=True)
+
+    def get_credentials():
+        cred = None
+        if os.path.exists("CredentialData.json") and os.path.getsize("CredentialData.json") > 0:
+            cred = Credentials.from_authorized_user_file("CredentialData.json", scopes)
+
+        if not cred or not cred.valid:
+            #If the auth token is expired
+            print("Getting new credentials")
+            if cred and cred.expired and cred.refresh_token:
+                cred.refresh(Request())
+            else:
+                cred = flow.run_local_server(port=0, open_browser=True)
+
+            print("Credentials saved")
+            with open("CredentialData.json", "w") as token:
+                token.write(cred.to_json())
+    
+        return cred
+
+    #check auth and refresh tokens
+    #if not auth_token or abs(curr_time - token_added_time).total_seconds() > 3600:
+        #os.putenv("AUTH_TOKEN", credentials.token)
+        #os.putenv("TOKEN_ADDED_TIME", curr_time)
+
+    #if not refresh_token:
+        #os.putenv("REFRESH_TOKEN", credentials.refresh_token)
+    #flow.run_local_console does not work, deprecated as of December 18, 2025
     #credentials = flow.run_console(authorization_code_message='Enter the authorization code: ')
 
     #if chrome_browser:
     #    chrome_browser.open_new(flow.redirect_uri)
     #else:
     #    webbrowser.open_new(flow.redirect_uri)
+
+    credentials = get_credentials()
 
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, credentials=credentials, developerKey=DEVELOPER_KEY)
